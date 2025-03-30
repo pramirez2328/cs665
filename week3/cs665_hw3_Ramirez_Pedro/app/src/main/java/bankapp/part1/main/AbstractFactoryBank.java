f7cae6f3816dc830c3ef692ee95a3c227effb342
Pedro Ramirez <prlara2000@gmail.com>
2025-03-30 13:12:17 -0500

    finish part 1

--- week3/cs665_hw3_Ramirez_Pedro/app/src/main/java/bankapp/part1/main/AbstractFactoryBank.java
+++ week3/cs665_hw3_Ramirez_Pedro/app/src/main/java/bankapp/part1/main/AbstractFactoryBank.java
@@ -11,21 +11,25 @@ public class AbstractFactoryBank {
     public static void main(String[] args) {
         System.out.println("PART1");
 
-        System.out.println("--Testing Basic version of Accounts--\n");
+        System.out.println("\n--Testing Basic version of Accounts--\n");
         AccountFactory baseFactory = new AccountFactory();
         CheckingAccount basicChecking = baseFactory.createCheckingAccount();
         basicChecking.accountType();
         SavingsAccount basicSavings = baseFactory.createSavingsAccount();
         basicSavings.accountType();
 
-        System.out.println("--Testing Bank of America version of Accounts--\n");
+        System.out.println("\n----------------------------------------------");
+
+        System.out.println("\n--Testing Bank of America version of Accounts--\n");
         AccountFactory boaFactory = new BOAFactory();
         CheckingAccount boaChecking = boaFactory.createCheckingAccount();
         boaChecking.accountType();
         SavingsAccount boaSavings = boaFactory.createSavingsAccount();
         boaSavings.accountType();
 
-        System.out.println("--Testing Chase version of Accounts--\n");
+        System.out.println("\n----------------------------------------------");
+
+        System.out.println("\n--Testing Chase version of Accounts--\n");
         AccountFactory chaseFactory = new ChaseFactory();
         CheckingAccount chaseChecking = chaseFactory.createCheckingAccount();
         chaseChecking.accountType();
