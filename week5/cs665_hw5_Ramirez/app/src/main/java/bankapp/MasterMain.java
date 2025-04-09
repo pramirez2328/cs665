4a68d70628ab356dacdfe0f6875fe9b5a79f5872
Pedro Ramirez <prlara2000@gmail.com>
2025-04-09 18:50:48 -0500

    feat: add template

---
+++ week5/cs665_hw5_Ramirez/app/src/main/java/bankapp/MasterMain.java
@@ -0,0 +1,14 @@
+package bankapp;
+
+
+public class MasterMain {
+    public static void main(String[] args) {
+        System.out.println("======== Running Part 1: Decorator Pattern ========");
+
+
+        System.out.println("\n======== Running Part 2: Composite Pattern ========");
+
+
+    }
+}
+
